643ccfd6d4dcc39a85ddf5e28b4e158fce57fc58
NetworkMonk <jamesplant@gmail.com>
2020-09-19 19:47:08 +0100

    Created empty dashboard with sign out button

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import StandardHead from "../../components/seo/StandardHead";
+import Nav from "../../components/nav/Nav";
+import Footer from "../../components/footer/Footer";
+import PageTitle from "../../components/common/PageTitle";
+import ContainerWide from "../../components/common/ContainerWide";
+import Button from "../../components/form/Button";
+import { useDispatch } from "react-redux";
+import getUserState from "../../app/store/GetUser";
+import { CorsRequest as Request } from "../../app/api/API";
+
+export default function Interest() {
+  const { userState } = getUserState();
+  const dispatch = useDispatch();
+
+  var signOut = () => {
+    // Attempt to delete the refresh cookie from the server
+    Request({
+      path: process.env.NEXT_PUBLIC_AUTH_SERVICE + "auth/refresh",
+      method: "delete",
+      success: function (code) {
+        if (code === 200) {
+          // Set userstate as an empty object
+          dispatch({
+            type: "USERAUTH",
+            payload: {},
+          });
+        }
+      },
+      error: function () {},
+    });
+  };
+
+  return (
+    <div className="page-container">
+      <StandardHead />
+      <Nav />
+      <ContainerWide>
+        <PageTitle>My Dashboard</PageTitle>
+        <Button onClick={signOut}>Sign Out</Button>
+      </ContainerWide>
+      <Footer />
+    </div>
+  );
+}
